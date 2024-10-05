b1185a252890009c95a62ac7355ce002536fed82
Ricardo Morales Paredes <inkcannon@gmail.com>
2024-10-05 17:27:13 -0600

    Update ChallengeApplication.java

--- src/main/java/mx/com/grupoasesores/challenge/ChallengeApplication.java
+++ src/main/java/mx/com/grupoasesores/challenge/ChallengeApplication.java
@@ -3,8 +3,10 @@ package mx.com.grupoasesores.challenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
