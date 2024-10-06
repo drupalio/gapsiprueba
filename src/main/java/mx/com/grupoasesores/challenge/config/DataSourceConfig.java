a499f4a063fd20116637631c142add3ddb241c61
Ricardo Morales Paredes <inkcannon@gmail.com>
2024-10-05 22:02:37 -0600

    Add Configuration

---
+++ src/main/java/mx/com/grupoasesores/challenge/config/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package mx.com.grupoasesores.challenge.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.url}")
+    private String connectionString;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(driver)
+                .url(connectionString)
+                .username(user)
+                .password(password)
+                .build();
+    }
+}
